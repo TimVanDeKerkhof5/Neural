46c515d979cd977175675798e318f0681a9a389f
Tim van de Kerkhof <kerkt02@Datamining02.intra.gelre.nl>
2017-11-21 11:37:07 +0000

    removed dnn from cnn file

--- cnn.py
+++ cnn.py
@@ -77,23 +77,6 @@ def normalizedata(df):
 	df = pd.DataFrame(x_s)
 	return df
 
-def dnn():
-	training_set = tf.contrib.learn.datasets.base.load_csv_without_header(filename='train.csv',target_dtype=np.int,features_dtype=np.float64)
-	test_set = tf.contrib.learn.datasets.base.load_csv_without_header(filename='test.csv',target_dtype=np.int,features_dtype=np.float64)
-	print(test_set)
-	feature_columns = [tf.feature_column.numeric_column("x",shape=[97])]
-
-	classifier = tf.estimator.DNNClassifier(feature_columns=feature_columns,hidden_units=[30,40,30],model_dir="/tmp/read_model")
-
-	train_input_fn = tf.estimator.inputs.numpy_input_fn(x={"x":np.array(training_set.data)},y=np.array(training_set.target),num_epochs=None,shuffle=True)
-
-	classifier.train(input_fn=train_input_fn,steps=2000)
-
-	test_input_fn = tf.estimator.inputs.numpy_input_fn(x={"x":np.array(test_set.data)},y=np.array(test_set.target),num_epochs=1,shuffle=False)
-
-	accuracy_score = classifier.evaluate(input_fn=test_input_fn)["accuracy"]
-	print("\nTest Accuracy: {0:f}\n".format(accuracy_score))
-
 def cnn(features, labels, mode):
 	input_layer = tf.reshape(features["x"], [-1, 97, 1, 1])
 # Convolutional Layer #1
@@ -167,7 +150,7 @@ def cnn(features, labels, mode):
 
   # Configure the Training Op (for TRAIN mode)
 	if mode == tf.estimator.ModeKeys.TRAIN:
-		optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.003)
+		optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.0001)
 		train_op = optimizer.minimize(loss=loss,global_step=tf.train.get_global_step())
 		return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
 
@@ -204,7 +187,7 @@ def main():
 		shuffle=True)
 	cnn_classifier.train(
 		input_fn=train_input_fn,
-		steps=20000,
+		steps=100000,
 		hooks=[logging_hook])
 
 	eval_input_fn = tf.estimator.inputs.numpy_input_fn(
